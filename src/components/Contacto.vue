2f6d1586dd3198fd668597b529e37ec78ba89cfd
Miguel Berrio <53589014+mabmab159@users.noreply.github.com>
2021-08-30 14:27:23 -0500

    Contacto

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -1,14 +1,38 @@
+p
 <template>
   <div class="consola" style="border-style: solid; display: grid">
     <div>
-      <h3>Contactame</h3>
-      <p class="test">Numero de contacto x aca :v.<span>&#160;</span></p>
+      <p style="color: rgb(0, 164, 0); margin-bottom: 0px">
+        MabMab@Desktop
+        <span style="color: rgb(191, 191, 0)">~/Portafolio</span>
+      </p>
+      <p class="test">$ run contacto&#160;<span>&#160;</span></p>
+      <br />
+      <br />
+      <div class="row">
+        <div id="consola1" class="row"></div>
+      </div>
     </div>
     <img class="imagen" src="../assets/chibi.png" alt="" />
   </div>
 </template>
 <script>
-export default {};
+export default {
+  mounted() {
+    let variable1 = document.getElementById("consola1");
+    setTimeout(() => {
+      variable1.innerHTML = `
+            <h4>#Contacto</h4>
+            <p>Contacto: {</p>
+            <p>Celular: (+51) 912-437-377</p>
+            <p>Email: miguelberrioh@gmail.com</p>
+            <p>Github: mabmab159</p>
+            <p>Descargar: <span><a href="../assets/Prueba.pdf" download="MiguelBerrio">test</a></span> </p>
+            <p>}</p>
+        `;
+    }, 2100);
+  },
+};
 </script>
 <style scoped>
 .consola {
@@ -34,16 +58,17 @@ export default {};
   position: absolute;
   background-color: black;
   right: 0;
-  animation: maquina 2s steps(27);
-  border-left: 1px solid white;
+  animation: maquina 2s steps(13);
 }
 
 @keyframes maquina {
-  to {
-    width: 00%;
-  }
-  from {
+  0% {
     width: 100%;
+    border-left: 1px solid white;
+  }
+  100% {
+    width: 0%;
+    border-left: 1px solid white;
   }
 }
 </style>
